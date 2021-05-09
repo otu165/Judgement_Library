90eee49f4916bfc9d311881b5363da0a065e76b9
Juyeon Yoon <otu165@naver.com>
2021-05-09 20:12:31 +0900

    feat(TabLayout): add to scrap

--- app/src/main/java/com/example/judgement/feature/navigation/scrap/ScrapFragment.kt
+++ app/src/main/java/com/example/judgement/feature/navigation/scrap/ScrapFragment.kt
@@ -5,10 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TableLayout
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.judgement.R
 import com.example.judgement.data.ScrapRvData
+import com.google.android.material.tabs.TabLayout
 
 class ScrapFragment : Fragment() {
     private lateinit var scrapView: View
@@ -24,15 +27,36 @@ class ScrapFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
+        // tab layout setup
+        val tab = view?.findViewById(R.id.tab_layout_scrap) as TabLayout
+        for (category in resources.getStringArray(R.array.bottom_navigation_category)) {
+            tab.addTab(tab.newTab().setText(category))
+        }
+
+        tab.addOnTabSelectedListener(object: TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                // TODO request server data
+                Toast.makeText(requireContext(), "request server data", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+            }
+
+        })
+
         // recycler view setup
         val rv = view?.findViewById(R.id.rv_scrap) as RecyclerView
-        val tempData = mutableListOf<ScrapRvData>( // Todo 실제 서버 데이터로 수정
+        val tempData = mutableListOf<ScrapRvData>( // TODO replace it to server data
             ScrapRvData("믿을 수 없는 사건", "인천시의 한 아파트에서 싸늘한 주검..", "21.01.19"),
             ScrapRvData("믿을 수 없는 사건", "인천시의 한 아파트에서 싸늘한 주검..", "21.01.19"),
             ScrapRvData("믿을 수 없는 사건", "인천시의 한 아파트에서 싸늘한 주검..", "21.01.19"),
             ScrapRvData("믿을 수 없는 사건", "인천시의 한 아파트에서 싸늘한 주검..", "21.01.19"),
             ScrapRvData("믿을 수 없는 사건", "인천시의 한 아파트에서 싸늘한 주검..", "21.01.19"),
-            ScrapRvData("믿을 수 없는 사건", "인천시의 한 아파트에서 싸늘한 주검..", "21.01.19"))
+            ScrapRvData("믿을 수 없는 사건", "인천시의 한 아파트에서 싸늘한 주검..", "21.01.19")
+        )
         val rvAdapter = ScrapRvAdapter(requireContext(), tempData)
 
         rv.adapter = rvAdapter
