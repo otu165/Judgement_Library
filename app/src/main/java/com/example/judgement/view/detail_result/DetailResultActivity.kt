362812d432094632850a445ee0337b3a22a5ff65
JungYeonHee <dusgml5328@gmail.com>
2021-06-07 12:26:21 +0900

    feat: Detailed inquiry of case law

--- app/src/main/java/com/example/judgement/view/detail_result/DetailResultActivity.kt
+++ app/src/main/java/com/example/judgement/view/detail_result/DetailResultActivity.kt
@@ -1,10 +1,17 @@
 package com.example.judgement.view.detail_result
 
+import android.content.ClipData
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.databinding.DataBindingUtil
 import com.example.judgement.R
+import com.example.judgement.data.Item
 import com.example.judgement.databinding.ActivityDetailResultBinding
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import org.jsoup.select.Elements
 
 class DetailResultActivity : AppCompatActivity() {
     private lateinit var binding: ActivityDetailResultBinding
@@ -16,6 +23,8 @@ class DetailResultActivity : AppCompatActivity() {
         binding.activity = this
 
         setOnClickListener()
+        MyAsyncTask().execute()
+
         // TODO 사용자의 스크랩여부에 따라 토글 버튼 Checked 여부 변경하기
     }
 
@@ -25,4 +34,79 @@ class DetailResultActivity : AppCompatActivity() {
             overridePendingTransition(R.anim.none, R.anim.exit_to_bottom)
         }
     }
+
+    //AsyncTask 정의
+    inner class MyAsyncTask: AsyncTask<String, String, ArrayList<String>>(){ //input, progress update type, result type
+        private var precId : String = "215415"
+
+        var judgementList: ArrayList<String> = arrayListOf()
+
+        override fun onPreExecute() { // background스레드를 실행하기전 준비 단계
+            super.onPreExecute()
+        }
+
+        override fun doInBackground(vararg params: String?): ArrayList<String> { // background 스레드로 일처리를 해주는 곳(UI 스레드와 별개로 작동)
+            // 웹에서 내용을 가져온다.
+            val doc: Document = Jsoup.connect("https://www.law.go.kr/LSW/precInfoP.do?precSeq=$precId&amp;mode=0").get()
+            doc.select("br").append("\n");
+
+            // 내용 중에서 원하는 부분을 가져온다.
+            // 사건번호
+            val case: Elements = doc.select("div.subtit1")
+            Log.d("elts", case.text()) // 사건번호
+
+            // 판시사항, 판결요지
+            val eltsSummary: Elements = doc.select("p.pty4")
+            Log.d("elts", eltsSummary[0].text())
+            Log.d("elts", eltsSummary[1].text())
+
+            // 원심판결, 주문, 이유
+            val eltsOrigin: Elements = doc.select("p.pty4_dep1")
+            Log.d("elts", eltsOrigin[5].text())
+            Log.d("elts", eltsOrigin[6].text())
+            Log.d("elts", eltsOrigin[7].text())
+
+            val judge: Elements = doc.select("div.pgroup div")
+            Log.d("elts", judge.text()) // 재판장
+            //Log.d("els", doc.title())
+
+            //  사건번호, 판시사항, 판결요지, 원심판결, 주문, 이유, 재판장
+            judgementList.add(case.text())
+            judgementList.add(eltsSummary[0].text())
+            judgementList.add(eltsSummary[1].text())
+            judgementList.add(eltsOrigin[5].text())
+            judgementList.add(eltsOrigin[6].text())
+            judgementList.add(eltsOrigin[7].text())
+            judgementList.add(judge.text())
+
+/*
+            elts.forEachIndexed{ index, elem ->
+                val a_href = elem.select("title")
+                //val thumb_img = elem.select("img").attr("src")
+                //val title = elem.select("strong.tit_thumb").text()
+                Log.d("index", a_href.toString())
+
+                //var judgement = Item()
+            }
+*/
+            return judgementList
+        }
+
+        override fun onPostExecute(result: ArrayList<String>?) { // background Thread가 일을 끝마치고 result 리턴
+            // 문서제목 출력
+            if (result != null) {
+                binding.txtDetailResultTitle.text = result[0]
+                binding.txtDetailResultCaseDescription.text = result[0]
+                binding.txtDetailResultCaseIssueDescription.text = result[1]
+                binding.txtDetailResultCaseSummaryDescription.text = result[2]
+                binding.txtDetailResultCaseOriginDescription.text = result[3]
+                binding.txtDetailResultCaseDecisionDescription.text = result[4]
+                binding.txtDetailResultCaseReasonDescription.text = result[5]
+                binding.txtDetailResultCaseJudgeDescription.text = result[6]
+            }
+            // tv_title.setText(result)
+
+        }
+    }
+
 }
